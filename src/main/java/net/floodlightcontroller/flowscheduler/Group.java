8ffd1c81af9fc55b5cfcb1029bc2e580b26d7d86
kunalmahajan <kunalmahajan@dyn-209-2-217-203.dyn.columbia.edu>
2016-09-14 14:12:48 -0400

    added equals and hashcode for comparison of group objects

--- src/main/java/net/floodlightcontroller/flowscheduler/Group.java
+++ src/main/java/net/floodlightcontroller/flowscheduler/Group.java
@@ -44,4 +44,26 @@ public class Group {
 	public U64 getBandwidthConsumed(){
 		return bwConsumed;
 	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Group other = (Group) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
 }
